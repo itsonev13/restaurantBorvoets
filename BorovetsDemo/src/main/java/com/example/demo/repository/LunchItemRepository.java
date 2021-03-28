2dc1f71e1eb73b6309e01d64e32292a38e7d5f3a
itsonev13 <i_tsonev13@abv.bg>
2021-03-28 22:40:45 +0300

    borovetts

--- BorovetsDemo/src/main/java/com/example/demo/repository/LunchItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.LunchItem;
-
-public interface LunchItemRepository extends JpaRepository<LunchItem, Integer> {
-
-}
